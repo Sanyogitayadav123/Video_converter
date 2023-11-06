d0529b79f1e9a2052ad1c924414c63d5163d8535
Sanyogita <sanyogitavkaps@gmail.com>
2023-11-06 13:26:44 +0530

    first commit

---
+++ src/pages/api/upload.js
@@ -0,0 +1,54 @@
+import multer from 'multer';
+import { exec } from 'child_process';
+import { promises as fs } from 'fs';
+
+const upload = multer({ dest: '/tmp' }); // Store the uploaded file temporarily in /tmp
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const convertAudio = async (filePath, outputFormat) => {
+      return new Promise((resolve, reject) => {
+        const outputPath = `/tmp/output.${outputFormat}`;
+        exec(`ffmpeg -i ${filePath} ${outputPath}`, (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(outputPath);
+          }
+        });
+      });
+    };
+
+    const fileHandlingMiddleware = upload.single('audioFile');
+
+    fileHandlingMiddleware(req, res, async (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'Error uploading file.' });
+      }
+
+      try {
+        const convertedFilePath = await convertAudio(req.file.path, 'mp3'); // Convert to mp3 for this example
+        const fileBuffer = await fs.readFile(convertedFilePath);
+
+        res.setHeader('Content-Disposition', 'attachment; filename=converted.mp3');
+        res.setHeader('Content-Type', 'audio/mpeg');
+        res.end(fileBuffer);
+
+        // Cleanup temp files
+        await fs.unlink(req.file.path);
+        await fs.unlink(convertedFilePath);
+      } catch (error) {
+        console.log('error', error)
+        res.status(500).json({ error: 'Error converting audio.' });
+      }
+    });
+  } else {
+    res.status(405).json({ error: 'Only POST requests are allowed.' });
+  }
+};
---
+++ src/pages/video.js
@@ -0,0 +1,42 @@
+
+import React, { useRef } from 'react';
+
+const VideoUploader = () => {
+  const fileRef = useRef(null);
+
+  const handleUpload = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('audioFile', fileRef.current.files[0]);
+
+    const response = await fetch('/api/upload', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.style.display = 'none';
+      a.href = url;
+      a.download = 'converted.mp3';
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    } else {
+      console.error('Failed to convert audio');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleUpload}>
+        <input type="file" ref={fileRef} accept="" required />
+        <button type="submit" className='p-2 bg-green-500 text-white'>Download</button>
+      </form>
+    </div>
+  );
+};
+
+export default VideoUploader;
