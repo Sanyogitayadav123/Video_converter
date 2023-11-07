238dfaa6fec5aaed1c34f66659e2daa54a86c3d7
Sanyogita <sanyogitavkaps@gmail.com>
2023-11-07 18:35:04 +0530

    add response msg

--- src/pages/video.js
+++ src/pages/video.js
@@ -5,30 +5,34 @@ const VideoUploader = () => {
   const fileRef = useRef(null);
 
   const handleUpload = async (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append('audioFile', fileRef.current.files[0]);
-
-    const response = await fetch('/api/upload', {
-      method: 'POST',
-      body: formData,
-    });
-
-    if (response.ok) {
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.style.display = 'none';
-      a.href = url;
-      a.download = 'converted.mp3';
-      document.body.appendChild(a);
-      a.click();
-      window.URL.revokeObjectURL(url);
-    } else {
-      console.error('Failed to convert audio');
+    try {
+      event.preventDefault();
+      const formData = new FormData();
+      formData.append('audioFile', fileRef.current.files[0]);
+  
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      console.log('response', response)
+      // if (response.ok) {
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'converted.mp3';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      // } else {
+        // console.error('Failed to convert audio');
+      // }
+    } catch (error) {
+      console.error('An error occurred:', error);
     }
   };
-
+  
   return (
     <div>
       <form onSubmit={handleUpload}>
